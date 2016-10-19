ba734aca78a83b00579ae41899d70a772baa1b08
Slammer-Jammer <slamjam@bk.ru>
2016-10-19 12:30:36 +0700

    Hello world

---
+++ snake/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 19.10.2016
+ * Time: 12:22
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace snake
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadLine();
+		}
+	}
+}

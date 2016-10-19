8220aaeb354ce8a713eba9e1c145645471d178a9
Slammer-Jammer <slamjam@bk.ru>
2016-10-19 16:41:09 +0700

    HLINE!

---
+++ snake/VLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace snake
+{
+	
+	public class VLine
+	{
+		List<Point> pList;
+		
+		public VLine(int x, int yUp, int yDown, char s)
+		{ pList = new List<Point>();
+			for (int y = yUp; y <= yDown; y++ )
+			 { Point p = new Point(x,y,s);
+				pList.Add(p);
+			 }
+		}
+		
+		public void Draw()
+		{	
+			foreach(Point p in pList)
+				p.Draw();
+		}
+	}
+}

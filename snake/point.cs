efc5ce6cf8376081f06440a838c35df0bc59fc2e
Slammer-Jammer <slamjam@bk.ru>
2016-10-19 15:22:44 +0700

    lesson3

---
+++ snake/point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace snake
+{
+	class Point
+	{
+		public int x;
+		public int y;
+		public char s;
+		
+		public void Draw()
+		{
+			Console.SetCursorPosition(x,y);
+			Console.Write(s);
+		}
+			
+	}
+}
